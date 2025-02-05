d557d0c8e13f6047dd3c9466f5f3515505129ac1
Roger Cao <xhrogercao@gmail.com>
2025-02-05 19:55:22 +0000

    Modify original file instead of separate files

--- pre-processing/pre-training/1-organize_comments.py
+++ pre-processing/pre-training/1-organize_comments.py
@@ -6,6 +6,7 @@ import re
 
 # The pattern for the months of archives to look for.
 ARCHIVE_MONTHS = "2017-1*"
+DATA_PATH = "data_test/raw"
 
 
 def main():
@@ -198,6 +199,8 @@ def main():
     path = "./"
     counts = {key: 0 for key in subreddit_to_topic.keys()}
     subreddit_regex = re.compile('"subreddit":"([^"]+)"')
+    postid_regex = re.compile('"id":"([^"]+)"')
+    linkid_regex = re.compile('"link_id":"([^"]+)"')
     for file in tqdm(
         list(
             glob(path + f"RC_{ARCHIVE_MONTHS}.zst")
@@ -219,27 +222,39 @@ def main():
             ):
                 # find the first subreddit mentioned
                 subreddit = subreddit_regex.search(line)
-                if subreddit is None:
+                postid = postid_regex.search(line)
+                linkid = linkid_regex.search(line)
+                if subreddit is None or postid is None:
                     continue
                 subreddit = subreddit.group(1)
+                postid = postid.group(1)
+                if linkid:
+                    linkid = linkid.group(1)[3:]
+                else:
+                    linkid = postid
                 if subreddit in subreddit_to_topic:
                     counts[subreddit] += 1
                     # write line to topic.txt file
                     for topic in subreddit_to_topic[subreddit]:
-                        os.makedirs("data/raw/" + topic, exist_ok=True)
+                        os.makedirs(f"{DATA_PATH}/" + topic, exist_ok=True)
+                        os.makedirs(
+                            f"{DATA_PATH}/{topic}/{subreddit}", exist_ok=True
+                        )
+                        os.makedirs(
+                            f"{DATA_PATH}/{topic}/{subreddit}/{linkid}",
+                            exist_ok=True,
+                        )
                         if "RC" in file:
                             with open(
-                                "data/raw/"
-                                + topic
-                                + "/"
-                                + subreddit
-                                + "-RC.txt",
+                                f"{DATA_PATH}/{topic}/{subreddit}"
+                                + f"/{linkid}/RC.txt",
                                 "a+",
                             ) as topic_file:
                                 topic_file.write(line)
                         else:
                             with open(
-                                "data/raw/" + topic + "/" + subreddit + ".txt",
+                                f"{DATA_PATH}/{topic}/{subreddit}"
+                                + f"/{linkid}/POST.txt",
                                 "a+",
                             ) as topic_file:
                                 topic_file.write(line)
