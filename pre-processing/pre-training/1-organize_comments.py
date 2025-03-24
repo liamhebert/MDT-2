7430e5eb89241632c9812292b303f98a0c95a531
Liam Hebert <liamhebert@gmail.com>
2025-03-24 17:26:13 +0000

    efficiently multi-threading files

--- pre-processing/pre-training/1-organize_comments.py
+++ pre-processing/pre-training/1-organize_comments.py
@@ -3,194 +3,230 @@ Processes .zst archives of Reddit comments and sorts them into
 topic-specific files.
 """
 
-import json
+import orjson 
 from tqdm import tqdm
 from glob import glob
 import os
 import re
+from heapq import heappush, heappop
+import threading
+
 
 # The pattern for the months of archives to look for.
-ARCHIVE_MONTHS = "2017-1*"
-DATA_PATH = "/mnt/DATA/reddit_share/data_test/raw"
+ARCHIVE_MONTHS = "2018-*"
+DATA_PATH = "./raw"
+KEEP_COUNT = 20000
 
+def extract_data(data: str, is_root: bool) -> dict:
+    """Extracts the data from a line of the Reddit comment file."""
+    data: dict = orjson.loads(data)
+    # print(data)
+    assert "data" not in data, data
+    
+    if not is_root:
+        # print(data)
+        # meta: dict = data["data"]
+        return {
+            "subreddit": data["subreddit"],
+            "id": data["id"],
+            "parent_id": data["parent_id"],
+            "link_id": data["link_id"],
+            "score": data["score"],
+            "body": data["body"],
+        }
+    else:
+        preview = data.get("preview", None)
+        if preview is not None:
+            preview = preview["images"][0]["source"]
+        return {
+            "subreddit": data["subreddit"],
+            "id": data["id"],
+            "score": data["score"],
+            "body": data.get("selftext", "<empty body>"),
+            "title": data["title"],
+            "preview": preview,
+        }
+
+def thread_decompress(index: int, file: str):
+    return os.system(f"zstd -d --long=31 -o ./tmp-{index} " + file)
 
 def main():
     """Entry point to process the files."""
-    # groups = {
-    #     "Partisan A": {
-    #         "a": [
-    #             "democrats",
-    #             "GunsAreCool",
-    #             "OpenChristian",
-    #             "GamerGhazi",
-    #             "excatholic",
-    #             "EnoughLibertarianSpam",
-    #             "AskAnAmerican",
-    #             "askhillarysupporters",
-    #             "liberalgunowners",
-    #             "lastweektonight",
-    #         ],
-    #         "b": [
-    #             "Conservative",
-    #             "progun",
-    #             "TrueChristian",
-    #             "KotakuInAction",
-    #             "Catholicism",
-    #             "ShitRConversativeSays",
-    #             "askaconservative",
-    #             "AskTrumpSupporters",
-    #             "Firearms",
-    #             "CGPGrey",
-    #         ],
-    #     },
-    #     "Partisan B": {
-    #         "a": [
-    #             "hillaryclinton",
-    #             "GamerGhazi",
-    #             "SandersForPresident",
-    #             "askhillarysupporters",
-    #             "BlueMidterm2018",
-    #             "badwomensanatomy",
-    #             "PoliticalVideo",
-    #             "liberalgunowners",
-    #             "GrassrootsSelect",
-    #             "GunsAreCool",
-    #         ],
-    #         "b": [
-    #             "The_Donald",
-    #             "KotakuInAction",
-    #             "HillaryForPrison",
-    #             "AskThe_Donald",
-    #             "PoliticalHumor",
-    #             "ChoosingBeggars",
-    #             "uncensorednews",
-    #             "Firearms",
-    #             "DNCleaks",
-    #             "dgu",
-    #         ],
-    #     },
-    #     "Affluence": {
-    #         "a": [
-    #             "vagabond",
-    #             "hitchhiking",
-    #             "DumpsterDiving",
-    #             "almosthomeless",
-    #             "AskACountry",
-    #             "KitchenConfidential",
-    #             "Nightshift",
-    #             "alaska",
-    #             "fuckolly",
-    #             "FolkPunk",
-    #         ],
-    #         "b": [
-    #             "backpacking",
-    #             "hiking",
-    #             "Frugal",
-    #             "personalfinance",
-    #             "travel",
-    #             "Cooking",
-    #             "fitbit",
-    #             "CampingandHiking",
-    #             "gameofthrones",
-    #             "IndieFolk",
-    #         ],
-    #     },
-    #     "Gender": {
-    #         "a": [
-    #             "AskMen",
-    #             "TrollYChromosome",
-    #             "AskMenOver30",
-    #             "OneY",
-    #             "TallMeetTall",
-    #             "daddit",
-    #             "ROTC",
-    #             "FierceFlow",
-    #             "malelivingspace",
-    #             "predaddit",
-    #         ],
-    #         "b": [
-    #             "AskWomen",
-    #             "CraftyTrolls",
-    #             "AskWomenOver30",
-    #             "women",
-    #             "bigboobproblems",
-    #             "Mommit",
-    #             "USMilitarySO",
-    #             "HaircareScience",
-    #             "InteriorDesign",
-    #             "BabyBumps",
-    #         ],
-    #     },
-    #     "Age": {
-    #         "a": [
-    #             "teenagers",
-    #             "youngatheists",
-    #             "teenrelationships",
-    #             "AskMen",
-    #             "saplings",
-    #             "hsxc",
-    #             "trackandfield",
-    #             "TeenMFA",
-    #             "bapccanada",
-    #             "RedHotChiliPeppers",
-    #         ],
-    #         "b": [
-    #             "RedditForGrownups",
-    #             "TrueAtheism",
-    #             "relationship_advice",
-    #             "AskMenOver30",
-    #             "eldertrees",
-    #             "running",
-    #             "trailrunning",
-    #             "MaleFashionMarket",
-    #             "canadacordcutters",
-    #             "pearljam",
-    #         ],
-    #     },
-    #     "Edgy": {
-    #         "a": [
-    #             "memes",
-    #             "watchpeoplesurvive",
-    #             "MissingPersons",
-    #             "twinpeaks",
-    #             "pickuplines",
-    #             "texts",
-    #             "startrekgifs",
-    #             "subredditoftheday",
-    #             "peeling",
-    #             "rapbattles",
-    #         ],
-    #         "b": [
-    #             "ImGoingToHellForThis",
-    #             "watchpeopledie",
-    #             "MorbidReality",
-    #             "TrueDetective",
-    #             "MeanJokes",
-    #             "FiftyFifty",
-    #             "DaystromInstitute",
-    #             "SRSsucks",
-    #             "Gore",
-    #             "bestofworldstar",
-    #         ],
-    #     },
-    # }
-
     groups = {
-        "Test-LocalCity": {
+        "Partisan A": {
             "a": [
-                "sanfrancisco",
-                "bayarea",
-                "toronto",
-                "vancouver",
-                "calgary",
-                "waterloo",
-                "texas",
-                "ohio",
-                "newyorkcity",
-                "losangeles",
+                "democrats",
+                "GunsAreCool",
+                "OpenChristian",
+                "GamerGhazi",
+                "excatholic",
+                "EnoughLibertarianSpam",
+                "AskAnAmerican",
+                "askhillarysupporters",
+                "liberalgunowners",
+                "lastweektonight",
             ],
-        }
+            "b": [
+                "Conservative",
+                "progun",
+                "TrueChristian",
+                "KotakuInAction",
+                "Catholicism",
+                "ShitRConversativeSays",
+                "askaconservative",
+                "AskTrumpSupporters",
+                "Firearms",
+                "CGPGrey",
+            ],
+        },
+        "Partisan B": {
+            "a": [
+                "hillaryclinton",
+                "GamerGhazi",
+                "SandersForPresident",
+                "askhillarysupporters",
+                "BlueMidterm2018",
+                "badwomensanatomy",
+                "PoliticalVideo",
+                "liberalgunowners",
+                "GrassrootsSelect",
+                "GunsAreCool",
+            ],
+            "b": [
+                "The_Donald",
+                "KotakuInAction",
+                "HillaryForPrison",
+                "AskThe_Donald",
+                "PoliticalHumor",
+                "ChoosingBeggars",
+                "uncensorednews",
+                "Firearms",
+                "DNCleaks",
+                "dgu",
+            ],
+        },
+        "Affluence": {
+            "a": [
+                "vagabond",
+                "hitchhiking",
+                "DumpsterDiving",
+                "almosthomeless",
+                "AskACountry",
+                "KitchenConfidential",
+                "Nightshift",
+                "alaska",
+                "fuckolly",
+                "FolkPunk",
+            ],
+            "b": [
+                "backpacking",
+                "hiking",
+                "Frugal",
+                "personalfinance",
+                "travel",
+                "Cooking",
+                "fitbit",
+                "CampingandHiking",
+                "gameofthrones",
+                "IndieFolk",
+            ],
+        },
+        "Gender": {
+            "a": [
+                "AskMen",
+                "TrollYChromosome",
+                "AskMenOver30",
+                "OneY",
+                "TallMeetTall",
+                "daddit",
+                "ROTC",
+                "FierceFlow",
+                "malelivingspace",
+                "predaddit",
+            ],
+            "b": [
+                "AskWomen",
+                "CraftyTrolls",
+                "AskWomenOver30",
+                "women",
+                "bigboobproblems",
+                "Mommit",
+                "USMilitarySO",
+                "HaircareScience",
+                "InteriorDesign",
+                "BabyBumps",
+            ],
+        },
+        "Age": {
+            "a": [
+                "teenagers",
+                "youngatheists",
+                "teenrelationships",
+                "AskMen",
+                "saplings",
+                "hsxc",
+                "trackandfield",
+                "TeenMFA",
+                "bapccanada",
+                "RedHotChiliPeppers",
+            ],
+            "b": [
+                "RedditForGrownups",
+                "TrueAtheism",
+                "relationship_advice",
+                "AskMenOver30",
+                "eldertrees",
+                "running",
+                "trailrunning",
+                "MaleFashionMarket",
+                "canadacordcutters",
+                "pearljam",
+            ],
+        },
+        "Edgy": {
+            "a": [
+                "memes",
+                "watchpeoplesurvive",
+                "MissingPersons",
+                "twinpeaks",
+                "pickuplines",
+                "texts",
+                "startrekgifs",
+                "subredditoftheday",
+                "peeling",
+                "rapbattles",
+            ],
+            "b": [
+                "ImGoingToHellForThis",
+                "watchpeopledie",
+                "MorbidReality",
+                "TrueDetective",
+                "MeanJokes",
+                "FiftyFifty",
+                "DaystromInstitute",
+                "SRSsucks",
+                "Gore",
+                "bestofworldstar",
+            ],
+        },
     }
+
+    # groups = {
+    #     "Test-LocalCity": {
+    #         "a": [
+    #             # "sanfrancisco",
+    #             # "bayarea",
+    #             "toronto",
+    #             # "vancouver",
+    #             # "calgary",
+    #             # "waterloo",
+    #             # "texas",
+    #             # "ohio",
+    #             # "newyorkcity",
+    #             # "losangeles",
+    #         ],
+    #     }
+    # }
 
     # a dictionary of the name of each subreddit in groups and the topic it
     # belongs to. If there are multiple topics, note each one
@@ -203,88 +239,117 @@ def main():
                 subreddit_to_topic[sub] += [topic]
 
     path = "/mnt/DATA/reddit_share/"
-    counts = {key: 0 for key in subreddit_to_topic.keys()}
-    subreddit_regex = re.compile('"subreddit":"([^"]+)"')
-    subreddit_null_regex = re.compile('"subreddit_id":null')
-    postid_regex = re.compile('"id":"([^"]+)"')
+    # path = "./"
     linkid_regex = re.compile('"link_id":"([^"]+)"')
-    crosspost_regex = re.compile('"crosspost_parent":".._([^"]+)"')
-    for file in tqdm(
-        list(
-            glob(path + f"RC_{ARCHIVE_MONTHS}.zst")
-            + list(glob(path + f"RS_{ARCHIVE_MONTHS}.zst"))
-        ),
-        desc="Files",
+    subreddit_regex = re.compile('"subreddit":"([^"]+)"')
+    
+    data_heaps = {sub: [] for sub in subreddit_to_topic.keys()}
+    current_thread = None
+    
+    files = list(enumerate(glob(path + f"RS_{ARCHIVE_MONTHS}.zst")))
+    current_thread = threading.Thread(target=thread_decompress, args=files[0])
+    current_thread.start()
+
+    def process_submissions(file: str, index: int):
+        date = os.path.basename(file).split("_")[-1].split(".")[0]
+        year = int(date[:4])
+        month = int(date[5:]) + (year * 12)
+
+        with open(f"tmp-{index}", "r") as f:
+            for i, line in tqdm(enumerate(f), position=1):
+                subreddit = subreddit_regex.search(line).groups()
+                is_valid = any(sub in subreddit_to_topic for sub in subreddit)
+                if not is_valid:
+                    continue
+
+                data = extract_data(line, is_root=True)
+
+                subreddit = data["subreddit"]
+                if subreddit not in subreddit_to_topic:
+                    continue
+
+                score = data["score"]
+                if score < 25:
+                    continue
+
+                heappush(data_heaps[subreddit], (score, month, i, data))
+                if len(data_heaps[subreddit]) > KEEP_COUNT:
+                    heappop(data_heaps[subreddit])
+        os.remove(f"./tmp-{index}")
+
+    for (i, file), (j, next_file) in tqdm(
+        zip(files, files[1:]),
+        desc="Reading submissions",
         position=0,
     ):
-        # print(file)
-        os.system("zstd -d -T24 --memory=2048MiB " + file)
-        with open(file[:-4], "r") as f:
-            # find the total number of lines in the file
-            num_lines = 0
-            for line in tqdm(f, desc="Counting Lines", position=1, leave=True):
-                num_lines += 1
-            f.seek(0)
-            for line in tqdm(
-                f, total=num_lines, desc="Lines", position=2, leave=True
-            ):
-                # find the first subreddit mentioned
-                subreddit = subreddit_regex.search(line)
-                postids = postid_regex.findall(line)
-                crosspost_ids = crosspost_regex.findall(line)
-                # Filter out crosspost ids from a list of possible ids
-                for crosspost_id in crosspost_ids:
-                    if crosspost_id in postids:
-                        postids.remove(crosspost_id)
-                # Assume post IDs have length less than 8. If not, it's not
-                # postid and we can remove it
-                for i in reversed(range(len(postids))):
-                    if len(postids[i]) > 8:
-                        postids.pop(i)
+        current_thread.join()
 
-                if subreddit is None:
-                    # If subreddit_id is null, assume it's an ad
-                    # In which case we can skip this post
-                    continue
-                subreddit = subreddit.group(1)
-                if len(postids) != 1:
-                    print(f"Incorrect IDs: {postids}")
-                    print(line)
-                    continue
-                postid = postids[0]
-                if "RC" in file:
-                    # Comments have link IDs as a field. Use that.
-                    linkid = linkid_regex.search(line)
-                    linkid = linkid.group(1)[3:]
-                else:
-                    # Original posts don't have link IDs, so we just use
-                    # the post ID as the link ID
-                    linkid = postid
-                if subreddit in subreddit_to_topic:
-                    counts[subreddit] += 1
-                    # write line to topic.txt file
-                    for topic in subreddit_to_topic[subreddit]:
-                        os.makedirs(
-                            f"{DATA_PATH}/{topic}/{subreddit}/{linkid}",
-                            exist_ok=True,
-                        )
-                        if "RC" in file:
-                            with open(
-                                f"{DATA_PATH}/{topic}/{subreddit}"
-                                + f"/{linkid}/RC.txt",
-                                "a+",
-                            ) as topic_file:
-                                topic_file.write(line)
-                        else:
-                            with open(
-                                f"{DATA_PATH}/{topic}/{subreddit}"
-                                + f"/{linkid}/POST.txt",
-                                "a+",
-                            ) as topic_file:
-                                topic_file.write(line)
-        os.remove(file[:-4])
+        current_thread = threading.Thread(target=thread_decompress, args=(j, next_file))
+        current_thread.start()
+        
+        process_submissions(file, i)
+    
+    current_thread.join()
+    # process the last file
+    process_submissions(files[-1][0], files[-1][1])
+    
 
-    json.dump(counts, open("counts.json", "w"))
+    # get link_ids from heap for each subreddit that survived
+    ids_to_keep = {sub: set() for sub in subreddit_to_topic.keys()}
+    for sub, heap in data_heaps.items():
+        print("Subreddit: ", sub, " Size: ", len(heap))
+        topic = subreddit_to_topic[sub][0]
+        # Start from a clean slate each time
+        if os.path.isdir(f"{DATA_PATH}/{topic}/{sub}"):
+            os.system(f"rm -r {DATA_PATH}/{topic}/{sub}")
+        
+        os.makedirs(f"{DATA_PATH}/{topic}/{sub}", exist_ok=True)
+
+        with open(f"{DATA_PATH}/{topic}/{sub}/POST.txt", "wb") as f:
+            for _, _, _, data in heap:
+                ids_to_keep[sub].add(data["id"])
+                f.write(orjson.dumps(data, option=orjson.OPT_APPEND_NEWLINE))
+    
+    del data_heaps
+    # now we can read the comments
+
+    files = list(enumerate(glob(path + f"RC_{ARCHIVE_MONTHS}.zst")))
+    current_thread = threading.Thread(target=thread_decompress, args=files[0])
+    current_thread.start()
+
+    def process_comments(index: int):
+        with open(f"./tmp-{index}", "r") as f:
+            for line in tqdm(f, position=1):
+                subreddit = subreddit_regex.search(line).group(1)
+                if not subreddit in subreddit_to_topic:
+                    continue
+
+                link_id = linkid_regex.search(line).group(1)[3:]
+                
+                if link_id in ids_to_keep[subreddit]:
+                    topic = subreddit_to_topic[subreddit][0]
+                    data = extract_data(line, is_root=False)
+                    with open(f"{DATA_PATH}/{topic}/{subreddit}/RC.txt", "ab") as f:
+                        f.write(orjson.dumps(data, option=orjson.OPT_APPEND_NEWLINE))
+        os.remove(f"./tmp-{index}")
+    
+
+    for (i, file), (j, next_file) in tqdm(
+        zip(files, files[1:]),
+        desc="Reading comments",
+        position=0,
+    ):
+        current_thread.join()
+
+        current_thread = threading.Thread(target=thread_decompress, args=(j, next_file))
+        current_thread.start()
+        
+        process_comments(i)
+    
+    current_thread.join()
+    # process the last file
+    process_comments(files[-1][0])
+
 
 
 if __name__ == "__main__":
