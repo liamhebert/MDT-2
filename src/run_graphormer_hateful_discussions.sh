7a4f72d35fe0f60ee14073cbed3639b3eb103f90
Max Hao <132418186+MaxHao56@users.noreply.github.com>
2025-11-21 14:45:30 -0500

    Fix command syntax in run_graphormer script

--- src/run_graphormer_hateful_discussions.sh
+++ src/run_graphormer_hateful_discussions.sh
@@ -14,5 +14,4 @@ export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
 export TOKENIZERS_PARALLELISM=false
 export HYDRA_FULL_ERROR=1
 
-# python train.py experiment=graphormer_hatefuldiscussions2 trainer=gpu logger=wandb logger.wandb.project=graphormer_hd2 env=all
-python train.py hydra/launcher=basic experiment=graphormer_hatefuldiscussions2 trainer=gpu logger=wandb logger.wandb.project=graphormer_hd2 env=all
+python train.py experiment=graphormer_hatefuldiscussions2 trainer=gpu logger=wandb logger.wandb.project=graphormer_hd2 env=all
